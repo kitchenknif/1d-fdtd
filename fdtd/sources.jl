8855400e397d56d04021c36ef81370a0ccd4289d
Pavel Dmitriev <pavel.a.dmitriev@gmail.com>
2016-02-28 19:25:44 +0800

    Task0 done

--- fdtd/sources.jl
+++ fdtd/sources.jl
@@ -3,7 +3,12 @@ module sources
 export hard_gaussian_source!
 
 function hard_gaussian_source!(ez, pos, time, delay=30., amplitude=100.)
-    ez[pos] = exp(-(time - delay) * (time - delay) / amplitude)
+    ez[pos] = exp(-(time - delay) * (time - delay) / amplitude);
 end
+
+function additive_gaussian_source!(ez, pos, time, delay=30, amplitude=100.)
+    ez[pos] += exp(-(time - delay) * (time - 30.) / 100.);
+end
+
 
 end
